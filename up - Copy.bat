56e1fd346c279ba534b388db180dfd486daa4bd0
Rk <rkramesh@users.noreply.github.com>
2016-08-31 18:56:39 +0530

    up.bat

--- up - Copy.bat
+++ up - Copy.bat
@@ -3,27 +3,29 @@ rem START TortoiseProc.exe /command:update /path:"C:\www\MyRepo\" /closeonend:0
 
 rem CD C:\Users\radhakrishnanr\Desktop\deployment
 
-
-START TortoiseProc.exe /command:update /path:"C:\Users\radhakrishnanr\Desktop\deployment\Services.AzMan.Authentication"  closeonend:1
-START TortoiseProc.exe /command:update /path:"C:\Users\radhakrishnanr\Desktop\deployment\Services.BatchCommStagingQueue"  closeonend:1
-START TortoiseProc.exe /command:update /path:"C:\Users\radhakrishnanr\Desktop\deployment\Services.Common.Scheduler"  closeonend:1
-START TortoiseProc.exe /command:update /path:"C:\Users\radhakrishnanr\Desktop\deployment\Services.Composite"  closeonend:1
-START TortoiseProc.exe /command:update /path:"C:\Users\radhakrishnanr\Desktop\deployment\Services.Correspondence"  closeonend:1
-START TortoiseProc.exe /command:update /path:"C:\Users\radhakrishnanr\Desktop\deployment\Services.CorrespondenceProcessor"  closeonend:1
-START TortoiseProc.exe /command:update /path:"C:\Users\radhakrishnanr\Desktop\deployment\Services.Credit"  closeonend:1
-START TortoiseProc.exe /command:update /path:"C:\Users\radhakrishnanr\Desktop\deployment\Services.Membership"  closeonend:1
-START TortoiseProc.exe /command:update /path:"C:\Users\radhakrishnanr\Desktop\deployment\Services.Messaging"  closeonend:1
-START TortoiseProc.exe /command:update /path:"C:\Users\radhakrishnanr\Desktop\deployment\Services.Package"  closeonend:1
-START TortoiseProc.exe /command:update /path:"C:\Users\radhakrishnanr\Desktop\deployment\Services.ServiceRequest.Business"  closeonend:1
-START TortoiseProc.exe /command:update /path:"C:\Users\radhakrishnanr\Desktop\deployment\Services.StandingData"  closeonend:1
-START TortoiseProc.exe /command:update /path:"C:\Users\radhakrishnanr\Desktop\deployment\Starburst"  closeonend:1
-START TortoiseProc.exe /command:update /path:"C:\Users\radhakrishnanr\Desktop\deployment\Workspaces"  closeonend:1
-START TortoiseProc.exe /command:update /path:"C:\Users\radhakrishnanr\Desktop\deployment\ai2"  closeonend:1
-START TortoiseProc.exe /command:update /path:"C:\Users\radhakrishnanr\Desktop\deployment\common2"  closeonend:1
-START TortoiseProc.exe /command:update /path:"C:\Users\radhakrishnanr\Desktop\deployment\Core.Components"  closeonend:1
-START TortoiseProc.exe /command:update /path:"C:\Users\radhakrishnanr\Desktop\deployment\Correspondence.Common"  closeonend:1
-START TortoiseProc.exe /command:update /path:"C:\Users\radhakrishnanr\Desktop\deployment\Engage"  closeonend:1
-START TortoiseProc.exe /command:update /path:"C:\Users\radhakrishnanr\Desktop\deployment\gadget_insurance"  closeonend:1
-START TortoiseProc.exe /command:update /path:"C:\Users\radhakrishnanr\Desktop\deployment\mastercardhub"  closeonend:1
-START TortoiseProc.exe /command:update /path:"C:\Users\radhakrishnanr\Desktop\deployment\pg_uk2"  closeonend:1
-START TortoiseProc.exe /command:update /path:"C:\Users\radhakrishnanr\Desktop\deployment\Services.Asset.Business"  closeonend:1
+@echo off
+FOR %%A IN (
+    "C:\Users\radhakrishnanr\Desktop\deployment
+    "C:\Users\radhakrishnanr\Desktop\deployment
+    "C:\Users\radhakrishnanr\Desktop\deployment
+    "C:\Users\radhakrishnanr\Desktop\deployment
+    "C:\Users\radhakrishnanr\Desktop\deployment
+    "C:\Users\radhakrishnanr\Desktop\deployment
+    "C:\Users\radhakrishnanr\Desktop\deployment
+    "C:\Users\radhakrishnanr\Desktop\deployment
+    "C:\Users\radhakrishnanr\Desktop\deployment
+    "C:\Users\radhakrishnanr\Desktop\deployment
+    "C:\Users\radhakrishnanr\Desktop\deployment
+    "C:\Users\radhakrishnanr\Desktop\deployment
+    "C:\Users\radhakrishnanr\Desktop\deployment
+    "C:\Users\radhakrishnanr\Desktop\deployment
+    "C:\Users\radhakrishnanr\Desktop\deployment
+    "C:\Users\radhakrishnanr\Desktop\deployment
+    "C:\Users\radhakrishnanr\Desktop\deployment
+    "C:\Users\radhakrishnanr\Desktop\deployment
+    "C:\Users\radhakrishnanr\Desktop\deployment
+    "C:\Users\radhakrishnanr\Desktop\deployment
+    "C:\Users\radhakrishnanr\Desktop\deployment
+    "C:\Users\radhakrishnanr\Desktop\deployment
+    "C:\Users\radhakrishnanr\Desktop\deployment
+) DO START TortoiseProc.exe /command:update /path:%%A /closeonend:1
