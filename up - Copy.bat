6d5a188b320313efdb0e6f6316cdf64f6595f59b
Rk <rkramesh@users.noreply.github.com>
2016-08-31 19:16:02 +0530

    up.bat

--- up - Copy.bat
+++ up - Copy.bat
@@ -1,9 +1,9 @@
-rem CD C:\Program Files\TortoiseSVN\bin\
+rem add TortoiseProc.exe to the path
 rem START TortoiseProc.exe /command:update /path:"C:\www\MyRepo\" /closeonend:0
 
 rem CD C:\Users\radhakrishnanr\Desktop\deployment
 
-
+rem navigate to the parent directory containing subfolders
 CD C:\Users\radhakrishnanr\Desktop\deployment
 
 @echo off
