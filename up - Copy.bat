d3de8816d40d8ed42ee8926bce0db2fc9fe7b62a
Rk <rkramesh@users.noreply.github.com>
2016-08-31 19:09:32 +0530

    changes to svn up

--- up - Copy.bat
+++ up - Copy.bat
@@ -3,29 +3,9 @@ rem START TortoiseProc.exe /command:update /path:"C:\www\MyRepo\" /closeonend:0
 
 rem CD C:\Users\radhakrishnanr\Desktop\deployment
 
+
+CD C:\Users\radhakrishnanr\Desktop\deployment
+
 @echo off
-FOR %%A IN (
-    "C:\Users\radhakrishnanr\Desktop\deployment
-    "C:\Users\radhakrishnanr\Desktop\deployment
-    "C:\Users\radhakrishnanr\Desktop\deployment
-    "C:\Users\radhakrishnanr\Desktop\deployment
-    "C:\Users\radhakrishnanr\Desktop\deployment
-    "C:\Users\radhakrishnanr\Desktop\deployment
-    "C:\Users\radhakrishnanr\Desktop\deployment
-    "C:\Users\radhakrishnanr\Desktop\deployment
-    "C:\Users\radhakrishnanr\Desktop\deployment
-    "C:\Users\radhakrishnanr\Desktop\deployment
-    "C:\Users\radhakrishnanr\Desktop\deployment
-    "C:\Users\radhakrishnanr\Desktop\deployment
-    "C:\Users\radhakrishnanr\Desktop\deployment
-    "C:\Users\radhakrishnanr\Desktop\deployment
-    "C:\Users\radhakrishnanr\Desktop\deployment
-    "C:\Users\radhakrishnanr\Desktop\deployment
-    "C:\Users\radhakrishnanr\Desktop\deployment
-    "C:\Users\radhakrishnanr\Desktop\deployment
-    "C:\Users\radhakrishnanr\Desktop\deployment
-    "C:\Users\radhakrishnanr\Desktop\deployment
-    "C:\Users\radhakrishnanr\Desktop\deployment
-    "C:\Users\radhakrishnanr\Desktop\deployment
-    "C:\Users\radhakrishnanr\Desktop\deployment
-) DO START TortoiseProc.exe /command:update /path:%%A /closeonend:1
+
+FOR /D %%A IN (*) DO START TortoiseProc.exe /command:update /path:%%A /closeonend:1
