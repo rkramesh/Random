dae1bf5124df8b68a0af1904617a737bf330d01c
Rk <rkramesh@users.noreply.github.com>
2016-09-02 17:12:01 +0530

    update comments

--- up - Copy.bat
+++ up - Copy.bat
@@ -7,5 +7,5 @@ rem navigate to the parent directory containing subfolders
 CD C:\Users\radhakrishnanr\Desktop\deployment
 
 @echo off
-
+rem id used in cmdline use single '%',i.e %A instead of %%A
 FOR /D %%A IN (*) DO START TortoiseProc.exe /command:update /path:%%A /closeonend:1
