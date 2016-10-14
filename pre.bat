671f879b99031b5bcddb6c8b5325fafd93f7f7e9
Rk <rkramesh@users.noreply.github.com>
2016-10-14 17:54:16 +0530

    Changes to Readme

--- pre.bat
+++ pre.bat
@@ -9,16 +9,13 @@ CD %home%
 FOR /D %%A IN (*) DO (
     CD %%A
 	echo Starting merge for branch %%A >> %log%/%d%-merge.txt 2>> %log%/%d%_mergeerror.txt
-	:set trunk="https://scm.virtusa.com/affinioncmb/branches/Release_1/Source/%%A"	
-	:set branch="https://scm.virtusa.com/affinioncmb/branches/RELEASE2/%%A"
-	echo merging https://scm.virtusa.com/affinioncmb/branches/RELEASE2/%%A to https://scm.virtusa.com/affinioncmb/branches/Release_1/Source/%%A
-	svn info "https://scm.virtusa.com/affinioncmb/branches/RELEASE2/%%A" | findstr /B URL >> %log%/%d%-merge.txt 2>> %log%/%d%_mergeerror.txt
-	svn info "https://scm.virtusa.com/affinioncmb/branches/Release_1/Source/%%A"  | findstr /B URL >> %log%/%d%-merge.txt 2>> %log%/%d%_mergeerror.txt
-	:svn merge %branch% %trunk% --accept "theirs-conflict"  >> %log%/%t%_mergelog.txt 2>> %log%/%t%_mergeerrorlog.txt >> %log%/%d%-merge.txt 2>> %log%/%d%_mergeerror.txt
-	svn merge "https://scm.virtusa.com/affinioncmb/branches/RELEASE2/%%A"  https://scm.virtusa.com/affinioncmb/branches/Release_1/Source/%%A --accept postpone >> %log%/%d%-merge.txt 2>> %log%/%d%_mergeerror.txt
-	REM https://scm.virtusa.com/affinioncmb/branches/RELEASE2/Affinion.LoyaltyBuild.Web.ClientPortal/
-	REM https://scm.virtusa.com/affinioncmb/branches/Release_1/Source/Affinion.LoyaltyBuild.Web.ClientPortal/
-	CD %home%
+	:set trunk="https://scm./branches/Release_1/Source/%%A"	
+	:set branch="https://scm./branches/RELEASE2/%%A"
+	echo merging https://scm.v/branches/RELEASE2/%%A to https://scm.virtusa.com/affinioncmb/branches/Release_1/Source/%%A
+	svn info "https:///RELEASE2/%%A" | findstr /B URL >> %log%/%d%-merge.txt 2>> %log%/%d%_mergeerror.txt
+	svn info "https:///branches/Source/%%A"  | findstr /B URL >> %log%/%d%-merge.txt 2>> %log%/%d%_mergeerror.txt
+	svn merge "https:///%%A"  https:/s/Release_1/Source/%%A --accept postpone >> %log%/%d%-merge.txt 2>> %log%/%d%_mergeerror.txt
+         CD %home%
 	)
 
 	
