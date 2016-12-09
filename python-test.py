376c74620c88c51ca66bdd90493495da0c1f2d7f
Rk <rkramesh@users.noreply.github.com>
2016-12-09 19:54:52 +0530

    changes

--- python-test.py
+++ python-test.py
@@ -1,6 +1,6 @@
 import sqlite3
 conn = sqlite3.connect('rk.db')
-print "Opened database successfully"
+
 ##conn.execute('''CREATE TABLE COMPANY
 ##       (ID INT PRIMARY KEY     NOT NULL,
 ##       NAME           TEXT    NOT NULL,
@@ -8,16 +8,13 @@ print "Opened database successfully"
 ##       ADDRESS        CHAR(50),
 ##       SALARY         REAL);''')
 
-conn.execute('''CREATE TABLE IF NOT EXISTS KURAL
-       (ID INT PRIMARY KEY,
-       ADIKAR         INT    NOT NULL,
-       TAMIL          TEXT     NOT NULL,
-       ENGLISH        TEXT     NOT NULL,
-       MUVAURAI       TEXT     NOT NULL,
-       KALAURAI       TEXT     NOT NULL,
-       SOLOURAI       TEXT     NOT NULL,
-       TAMTRANS       TEXT     NOT NULL);''')
-conn.commit()       
+##conn.execute('''CREATE TABLE IF NOT EXISTS CHAPTER
+##       (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+##       ADIKAR         INT    NOT NULL,
+##       TAMIL          TEXT     NOT NULL,
+##       ENGLISH        TEXT     NOT NULL,
+##       ENGTRANS       TEXT     NOT NULL);''')
+##conn.commit()       
 
 #create database for tku
 
@@ -34,18 +31,30 @@ conn.commit()
 ##conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
 ##      VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 )");
 ##
+##SELECT CHAPTER.Tamil AS ADIKARAM,KURAL.TAMIL as THIRUKURAL FROM 'CHAPTER' LEFT join 'KURAL' ON CHAPTER.Adikar=KURAL.ADIKAR
+##alltamil
+##SELECT chapter.tamil,kural.tamil,kural.muvaurai,kural.kalaurai,kural.solourai FROM 'CHAPTER' LEFT join 'KURAL' ON CHAPTER.Adikar=KURAL.ADIKAR limit 10
+
+#SELECT chapter.tamil,kural.tamil,kural.muvaurai,kural.kalaurai,kural.solourai FROM 'CHAPTER' LEFT join 'KURAL' ON CHAPTER.Adikar=KURAL.ADIKAR order by RANDOM() limit 1
 ##conn.commit()
 ##print "Records created successfully";
 ##conn.close()
 ##cursor = conn.execute("SELECT id, name, address, salary  from COMPANY")
 cursor = conn.cursor()
 ##cursor.execute("DELETE FROM KURAL WHERE ID > -1;")
-##cursor.execute("drop table KURAL")
+##cursor.execute("drop table CHAPTER")
 ##cursor.execute("delete from KURAL  where ADIKAR='look'")
-cursor.execute("select * from KURAL")
+cursor.execute("SELECT chapter.tamil,kural.tamil,kural.muvaurai,kural.kalaurai,kural.solourai FROM 'CHAPTER' LEFT join 'KURAL' ON CHAPTER.Adikar=KURAL.ADIKAR limit 10")
 conn.commit()
 for row in cursor:
-    print row
+    print "\n"
+    print 'ATHIKARAM : '+row[0]
+    print row[1]
+    print 'MuvaUrai :'
+    print row[2]
+    
+    
+    
 conn.close()
 
 ##for row in cursor:
