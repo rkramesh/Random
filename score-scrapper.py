244a46cd54a68915fb45158fabd8c16060194f82
Rk <rkramesh@users.noreply.github.com>
2016-12-20 18:29:52 +0530

    changes

--- score-scrapper.py
+++ score-scrapper.py
@@ -8,6 +8,7 @@ class Score(object):
     def __init__(self):
         #will be using it for enhancements
         pass
+   
     @staticmethod
     def getMatch():
         url='http://www.cricbuzz.com/api/html/matches-menu'
@@ -20,77 +21,57 @@ class Score(object):
         soup = bs4.BeautifulSoup(response.content, "html.parser")
         optionlist={}
         for count,tag in enumerate (soup.find_all('li',{'class':re.compile('cb-lst-mtch')})):
-            print ("select '{}' for '{}' ".format(count,tag.a['title']))
+            print ("Enter '{}' for '{}' ".format(count,tag.a['title']))
             optionlist[count] = tag.a['href']
- 
-        choice = int(raw_input("> "))
-        
+        choice = int(raw_input("\n>Enter Your Option here: "))
         if choice in optionlist:
-           
-            print optionlist[choice]
-            
+           return Score.getScore('http://www.cricbuzz.com'+optionlist[choice])
         else:
-            pass
+            print'Wrong Option,Select again'
+            Score.getMatch()
+    @staticmethod
+    def getScore(url):
+        while True:
             
-      
-    def getScore(self):
-        url='http://www.cricbuzz.com/live-cricket-scores/16872/ind-vs-eng-5th-test-england-tour-of-india-2016-17'
-        response = requests.get(url,
-                                headers={'User-agent': 'Mozilla/5.0 (Windows NT '
-                                                       '6.2; WOW64) AppleWebKit/'
-                                                       '537.36 (KHTML, like '
-                                                       'Gecko) Chrome/37.0.2062.'
-                                                       '120 Safari/537.36'})
-        soup = bs4.BeautifulSoup(response.content, "html.parser")
-        print soup.title.text
-        print soup.find('div',{'class':re.compile('cb-mini-col')} ).text.strip()+'\n'
+            response = requests.get(url,
+                                    headers={'User-agent': 'Mozilla/5.0 (Windows NT '
+                                                           '6.2; WOW64) AppleWebKit/'
+                                                           '537.36 (KHTML, like '
+                                                           'Gecko) Chrome/37.0.2062.'
+                                                           '120 Safari/537.36'})
+            soup = bs4.BeautifulSoup(response.content, "html.parser")
+            
+            print '\n'+soup.title.text+'\n'
 
-    def getYaml(self):
-       
-       url='http://push.cricbuzz.com/match-api/16872/commentary.json'
-       response = requests.get(url,
-                            headers={'User-agent': 'Mozilla/5.0 (Windows NT '
-                                                   '6.2; WOW64) AppleWebKit/'
-                                                   '537.36 (KHTML, like '
-                                                   'Gecko) Chrome/37.0.2062.'
-                                                 '120 Safari/537.36'})
-       rk=response.content
-       data=json.loads(rk)
-##       pprint(data)
-       for tag in data['comm_lines']:
-           if 'comm' in tag.keys(): print  "\n".join(tag['comm'].split("<br/>"))
-               
-       
-        #rk = "\n".join(rk.split("<br />"))
-
-    def scoreCard(self):
-        url='http://www.cricbuzz.com/api/html/cricket-scorecard/16872'
-        response = requests.get(url,
-                            headers={'User-agent': 'Mozilla/5.0 (Windows NT '
-                                                   '6.2; WOW64) AppleWebKit/'
-                                                   '537.36 (KHTML, like '
-                                                   'Gecko) Chrome/37.0.2062.'
-                                                 '120 Safari/537.36'})
-        rk=response.content
-        soup = bs4.BeautifulSoup(response.content, "html.parser")
-##        print soup
-        print soup.find_all('div',{'class':re.compile('cb-col-100')})
-##        print soup.find('div',{'class':re.compile('.*cb-min-bat-rw')}).text.strip()+'\n'
-       
-##      
-##        
-##        for tag in soup.find_all(re.compile('.')):
-##            try:
-####               print tag.div.find('div',{'class':re.compile('.*cb-ovr-num')}).text+' '+tag.p.text
-##                print tag.div.find('div',{'class':re.compile('ccb-mini-col')})
-##            except AttributeError:
-##               pass
-##                
-##while True:
-##    cricbuzz=score()
-##    cricbuzz.getScore()
-##    time.sleep(15)
-
+            if soup.find('div',{'class':re.compile('cb-mini-col')}) :
+                print soup.find('div',{'class':re.compile('cb-mini-col')} ).text.strip()+'\n'
+                match='live'
+            else:
+                match='past'
+            duplicate=''
+            for tag in soup.find_all(re.compile('.')):
+                try:
+                    if match == 'live':
+                        print tag.div.find('div',{'class':re.compile('.*cb-ovr-num')}).text+' '+tag.p.text
+                    elif match == 'past':
+                        if len(tag.p.text) == duplicate:
+                            continue
+                        else:
+                            print tag.p.text+'\n'
+                            duplicate=len(tag.p.text)
+                        
+                        
+                except:
+                    pass
+                
+            if match == 'past':
+                break
+            else:
+                time.sleep(12)
+                    
+                    
+                
+            
 
 Score.getMatch()
     
